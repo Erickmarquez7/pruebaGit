d70fb9f28accc36652b54102b4d4551b88db7cd6
Erick <erick07@ciencias.unam.mx>
2021-05-10 16:04:34 -0500

    version 2

--- prueba.java
+++ prueba.java
@@ -1,6 +1,7 @@
 public class{
     public static void main(String[] args){
         System.out.println("Me quiero morir");
-        System.out.println("Version 1.0.0");
+        System.out.println("Version 1.0.1");
+        System.out.println("Me quiero morir v2.0");
     }
 }
